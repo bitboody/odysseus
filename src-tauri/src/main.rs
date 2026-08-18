f82284e8ca9738bd341f644c013955b12482c91d
Boody <bodebi639@gmail.com>
2026-08-18 22:11:50 +0300

    refactor: reorganize OS check and enhance installation state logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,17 +2,22 @@
 use std::path::Path;
 
 fn main() {
+    println!("{}", check_installation_state(check_os()));
     odysseus_lib::run();
+}
+
+fn check_os() -> &'static str {
+    return std::env::consts::OS;
 }
 
 fn check_installation_state(os: &str) -> bool {
     let config_file_path = match os {
-        "windows" => {
-            std::env::var("USERPROFILE").ok().map(|val| format!("{}\\Documents\\Odysseus\\config.json", val))
-        }
-        "macos" | "linux" => {
-            std::env::var("HOME").ok().map(|val| format!("{}/Documents/Odysseus/config.json", val))
-        }
+        "windows" => std::env::var("USERPROFILE")
+            .ok()
+            .map(|val| format!("{}\\Documents\\Odysseus\\config.json", val)),
+        "macos" | "linux" => std::env::var("HOME")
+            .ok()
+            .map(|val| format!("{}/Documents/Odysseus/config.json", val)),
         _ => None,
     };
 
